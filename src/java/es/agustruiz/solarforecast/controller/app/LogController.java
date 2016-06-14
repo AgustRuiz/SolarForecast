3facf88b9ecf253ac94a4d1e52bb7a7c2f35e7a0
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2016-06-14 18:03:06 +0200

    [ADD] LogLine.java [ADD] Test log view lines

--- src/java/es/agustruiz/solarforecast/controller/app/LogController.java
+++ src/java/es/agustruiz/solarforecast/controller/app/LogController.java
@@ -5,7 +5,10 @@
  */
 package es.agustruiz.solarforecast.controller.app;
 
+import es.agustruiz.solarforecast.model.LogLine;
 import es.agustruiz.solarforecast.service.ForecastService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LogController {
     
+    public static final String LOG_TAG = LogController.class.getName();
+    
     @RequestMapping(value = "/log", method = RequestMethod.GET)
     public String log(Model model) {
         model.addAttribute("projectName", "SolarForecast");
         model.addAttribute("title", "Log");
         model.addAttribute("navActiveItem", "log");
+        
+        List<LogLine> logList = new ArrayList<>();
+        logList.add(new LogLine(LOG_TAG, LogLine.DEBUG, "First debug test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.DEBUG, "Second debug test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.INFO, "First info test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.INFO, "Second info test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.INFO, "Third info test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.WARNING, "First warning test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.WARNING, "Second warning test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.WARNING, "Third warning test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.WARNING, "Forth warning test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.ERROR, "First danger test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.ERROR, "Second danger test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.ERROR, "Third danger test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.ERROR, "Forth danger test line"));
+        logList.add(new LogLine(LOG_TAG, LogLine.ERROR, "Fifth danger test line"));
+        
+        model.addAttribute("logList", logList);
+        
         return ("log");
     }
 }
--- web/WEB-INF/jsp/logContent.jsp
+++ web/WEB-INF/jsp/logContent.jsp
@@ -4,22 +4,40 @@
     Author     : Agustin Ruiz Linares <arl00029@red.ujaen.es>
 --%>
 <%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core' %>
 <h1 class="page-header">${title}</h1>
 <div class="table-responsive">
-    <table class="table table-striped">
+    <table class="table">
         <thead>
             <tr>
-                <th>From</th>
+                <th>Time/date</th>
                 <th>Level</th>
+                <th>From</th>
                 <th>Message</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            <c:if test="${not empty logList}">
+                <c:forEach var="item" varStatus="status" items="${logList}">
+                    <c:choose>
+                        <c:when test="${item.mode == 'D'}"><tr class="success"></c:when>
+                        <c:when test="${item.mode == 'I'}"><tr class="info"></c:when>
+                        <c:when test="${item.mode == 'W'}"><tr class="warning"></c:when>
+                        <c:when test="${item.mode == 'E'}"><tr class="danger"></c:when>
+                        <c:otherwise><tr></c:otherwise>
+                        </c:choose>
+                        <td>${item.timeDate}</td>
+                        <td>${item.mode}</td>
+                        <td>${item.from}</td>
+                        <td>${item.message}</td>
+                    </tr>
+                </c:forEach>
+            </c:if>
+            <c:if test="${empty logList}">
+                <tr>
+                    <td class="text-center" colspan="4"><em>Log is empty</em></td>
+                </tr>
+            </c:if>
         </tbody>
     </table>
 </div>
